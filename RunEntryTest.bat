c0a0854d704ef2cb7f6f7006a3b20a90ee9b77b9
yukutonet <amaji.yukuto@gmail.com>
2023-02-04 20:35:42 +0900

    Windows環境向けのテスト Entry ビルドスクリプト追加

---
+++ RunEntryTest.bat
@@ -0,0 +1,4 @@
+SET ENTRY=.\test\tmp_entry\tmpentry.cpp
+for /f "usebackq delims=" %%A in (`"c:\01_add\005_dev\020_msys2\tools\msys64\usr\bin\find.exe" ./src/ -name *.cpp -print0 ^| xargs -0 echo`) do set SRCS=%%A
+g++ %ENTRY% %SRCS% -o debug\test.out
+pause
