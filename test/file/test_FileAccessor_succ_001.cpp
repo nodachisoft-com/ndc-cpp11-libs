3b748ff49e2d4450d440be29f7d6e2d02eb7d938
yukutonet <amaji.yukuto@gmail.com>
2023-02-05 20:19:50 +0900

    ファイルアクセッサのテストを追加

---
+++ test/file/test_FileAccessor_succ_001.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+#include "../../src/ndclibs.hpp"
+
+#include <string>
+
+// デバッグ用ディレクトリを設定
+const std::string TESTTMP_DIR("./debug/");
+
+//
+TEST(FileAccessor, curDir_case1)
+{
+}
