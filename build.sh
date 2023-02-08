a92477cdb387784b0805a769fe4cbdd7edf75d3f
yukutonet <amaji.yukuto@gmail.com>
2023-02-08 22:38:35 +0900

    build shell 更新

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 # Clean
 rm -rf ./debug/*
+rm -rf ./publish/*
 
 # create compile script
 cd ./debug
@@ -10,3 +11,6 @@ find ../src/ -name *.cpp | xargs gcc -c
 # Create DLL
 ar rcs libNdclibs.a *.o
 rm -f *.o
+
+# copy to publish dirs
+mv *a ../publish
