2b80f25100d8c6ec9fddffd70ee468c06ae34f89
yukutonet <amaji.yukuto@gmail.com>
2023-02-15 01:33:45 +0900

    Fix: 画像印字テストケースを修正

--- test/image/test_bmp_debugfont_succ_001.cpp
+++ test/image/test_bmp_debugfont_succ_001.cpp
@@ -12,18 +12,13 @@ TEST(BitmapImage, writeChar_case001)
   image.clear(BASE_COLOR);
 
   ColorRGB COLOR_RED{0xff, 0x00, 0x00}; // 赤
-  // for (int v = -20; v < 71; v += 15)
-  //{
-  //   for (int u = -20; u < 71; u += 15)
-  //   {
-  //  image.writeChar(u, v, 'A', COLOR_RED);
-  image.writeChar(10, 10, 'E', COLOR_RED);
-  image.writeChar(20, 10, 'F', COLOR_RED);
-  image.writeChar(30, 10, 'G', COLOR_RED);
-  image.writeChar(40, 10, 'H', COLOR_RED);
-
-  //  }
-  //}
+  for (int v = -20; v < 71; v += 15)
+  {
+    for (int u = -20; u < 71; u += 15)
+    {
+      image.writeChar(u, v, (char)('A' + (u * u + v * v) % 20), COLOR_RED);
+    }
+  }
 
   image.WriteBmp(TESTTMP_DIR + "test_writeChar_case001.bmp");
 }
