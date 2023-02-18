0fa75fce782ee5f5a3744e65200d1e3c736f4863
yukutonet <amaji.yukuto@gmail.com>
2023-02-18 15:51:06 +0900

    差分開発用の runtest.sh を実験用に作成

--- runtest.sh
+++ runtest.sh
@@ -11,5 +11,12 @@ find ./src/ -type d | xargs -I{} mkdir -p ./tmp/compile/{}
 find ./src/ -name *.cpp | xargs -I{} gcc -c {} -o ./tmp/compile/{}.o
 find ./test/ -type d | xargs -I{} mkdir -p ./tmp/compile/{}
 find ./test/ -name test_*.cpp | xargs -I{} gcc -c {} -o ./tmp/compile/{}.o ${TEST_INCDIR}
-find ./tmp/compile/ -name *.o -print0 | xargs -0 | xargs -I{} ${CC} ${CFLAGS} {} -o ./tmp/compile/test.exe ${TEST_LIBDIR} ${TEST_LIBS}
-./tmp/compile/test.exe
+
+# create compile command
+printf "${CC}  ${CFLAGS} " > c.txt
+find ./tmp/compile/ -name *.o -print0 | xargs -0 | xargs -I{} printf {} >> c.txt
+printf " -o ./debug/test.exe " >> c.txt
+printf " ${TEST_LIBDIR} ${TEST_LIBS}" >> c.txt
+sh c.txt
+#find ./tmp/compile/ -name *.o -print0 | xargs -0 | xargs -I{} ${CC} ${CFLAGS} {} -o ./debug/test.exe ${TEST_LIBDIR} ${TEST_LIBS}
+./debug/test.exe
