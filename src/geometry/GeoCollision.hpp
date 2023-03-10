c4372ab86be2cb154ec8308a38590e816ceaa0c3
amaji <49400305+nodachisoft-com@users.noreply.github.com>
2023-03-10 14:36:28 +0000

    Collision に Point2d や LineSegment2d の Alias を追加

--- src/geometry/GeoCollision.hpp
+++ src/geometry/GeoCollision.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include "Point2d.hpp"
+#include "LineSegment2d.hpp"
 
 namespace nl
 {
@@ -17,6 +19,16 @@ namespace nl
                                                     const float t1x, const float t1y,
                                                     const float t2x, const float t2y);
 
+    /// @brief 点p(x,y) と、線分 ( 点t1(x,y) - 点t2(x,y) ) の最短距離の２乗を返す
+    /// @param p 点p
+    /// @param line 線分
+    /// @return 点p(x,y) と、線分 ( 点t1(x,y) - 点t2(x,y) ) の最短距離の２乗
+    static float powOfDistance_Point_To_LineSegment(const Point2d p,
+                                                    const LineSegment2d line)
+    {
+      return powOfDistance_Point_To_LineSegment(p.x, p.y, line.a.x, line.a.y, line.b.x, line.b.y);
+    }
+
     /// @brief 点p(x,y) と、線分 ( 点t1(x,y) - 点t2(x,y) ) の最短距離が、指定した距離内かを判定する
     /// @param px 点p の x 成分
     /// @param py 点p の y 成分
@@ -29,6 +41,17 @@ namespace nl
                                                         const float t1x, const float t1y,
                                                         const float t2x, const float t2y, const float condDistance);
 
+    /// @brief 点p(x,y) と、線分 ( 点t1(x,y) - 点t2(x,y) ) の最短距離が、指定した距離内かを判定する
+    /// @param p 点p
+    /// @param line 線分
+    /// @return 指定した距離の内部なら true を返す
+    static bool isInsideOfDistance_Point_To_LineSegment(const Point2d p,
+                                                        const LineSegment2d line,
+                                                        const float condDistance)
+    {
+      return isInsideOfDistance_Point_To_LineSegment(p.x, p.y, line.a.x, line.a.y, line.b.x, line.b.y, condDistance);
+    }
+
     /// @brief 点p(x,y) と 円 ( 中心点c(x,y)、半径 r ) までの距離の２乗を計算する
     /// @param px 点p の x 成分
     /// @param py 点p の y 成分
@@ -40,6 +63,18 @@ namespace nl
                                                const float cx, const float cy,
                                                const float r);
 
+    /// @brief 点p(x,y) と 円 ( 中心点c(x,y)、半径 r ) までの距離の２乗を計算する
+    /// @param p 点p
+    /// @param c 円の中心点 c
+    /// @param r 円の半径
+    /// @return 点p(x,y) と 円 ( 中心点c(x,y)、半径 r ) までの距離の２乗を返す
+    static float powOfDistance_Point_To_Circle(const Point2d p,
+                                               const Point2d c,
+                                               const float r)
+    {
+      return powOfDistance_Point_To_Circle(p.x, p.y, c.x, c.y, r);
+    }
+
     /// @brief 点p(x,y) に、円 ( 中心点c(x,y)、半径 r ) が含まれているかを判定する
     /// @param px 点p の x 成分
     /// @param py 点p の y 成分
@@ -50,5 +85,17 @@ namespace nl
     static bool isInsideOfDistance_Point_To_Circle(const float px, const float py,
                                                    const float cx, const float cy,
                                                    const float r);
+
+    /// @brief 点p(x,y) に、円 ( 中心点c(x,y)、半径 r ) が含まれているかを判定する
+    /// @param p 点p
+    /// @param c 円の中心点 c
+    /// @param r 円の半径
+    /// @return 点が円に含まれているなら true。境界値も含む。
+    static bool isInsideOfDistance_Point_To_Circle(const Point2d p,
+                                                   const Point2d c,
+                                                   const float r)
+    {
+      return isInsideOfDistance_Point_To_Circle(p.x, p.y, c.x, c.y, r);
+    }
   };
 };
